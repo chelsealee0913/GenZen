2d61253e9bfbdfd5d056293e0fb9bd1a1e95e736
chelsealee0913 <chelsea.lee0913@gmail.com>
2025-09-18 11:07:08 -0400

    Files changed
    Deleted Replit files and added Supabase files

--- client/src/pages/Community.tsx
+++
@@ -1,9 +0,0 @@
-import { CommunitySection } from "@/components/community/CommunitySection";
-
-export default function Community() {
-  return (
-    <div className="min-h-screen pb-24 md:pb-8">
-      <CommunitySection />
-    </div>
-  );
-}
--- client/src/pages/Library.tsx
+++
@@ -1,9 +0,0 @@
-import { MeditationLibrary } from "@/components/library/MeditationLibrary";
-
-export default function Library() {
-  return (
-    <div className="min-h-screen pb-24 md:pb-8">
-      <MeditationLibrary />
-    </div>
-  );
-}
